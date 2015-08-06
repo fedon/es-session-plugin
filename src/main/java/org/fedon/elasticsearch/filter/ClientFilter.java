3a12733ef6b3c69eacf725797f3b51e6deef55f8
Dmytro <fedongm@gmail.com>
2015-08-06 20:22:45 +0200

    REST Session check

---
+++ src/main/java/org/fedon/elasticsearch/filter/ClientFilter.java
@@ -0,0 +1,26 @@
+package org.fedon.elasticsearch.filter;
+
+import java.io.IOException;
+
+import javax.ws.rs.client.ClientRequestContext;
+import javax.ws.rs.client.ClientRequestFilter;
+
+/**
+ * @author Dmytro Fedonin
+ *
+ */
+public class ClientFilter implements ClientRequestFilter {
+    static ThreadLocal<String> cookieLocal = new ThreadLocal<>();
+
+    public static void setCookie(String cookie) {
+        cookieLocal.set(cookie);
+    }
+
+    @Override
+    public void filter(ClientRequestContext requestContext) throws IOException {
+        String cookie = cookieLocal.get();
+        if (cookie != null && cookie.length() > 0) {
+            requestContext.getHeaders().add("Cookie", cookie);
+        }
+    }
+}
--- src/main/java/org/fedon/elasticsearch/filter/SessionFilter.java
+++ src/main/java/org/fedon/elasticsearch/filter/SessionFilter.java
@@ -1,11 +1,19 @@
 package org.fedon.elasticsearch.filter;
 
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+
 import org.elasticsearch.common.logging.ESLogger;
 import org.elasticsearch.common.logging.Loggers;
 import org.elasticsearch.rest.RestChannel;
 import org.elasticsearch.rest.RestFilter;
 import org.elasticsearch.rest.RestFilterChain;
 import org.elasticsearch.rest.RestRequest;
+import org.elasticsearch.rest.RestRequest.Method;
+import org.glassfish.jersey.client.ClientConfig;
+import org.glassfish.jersey.client.proxy.WebResourceFactory;
+
+import com.cognoscience.api.skillcollider.LoginREST;
 
 /**
  * @author Dmytro Fedonin
@@ -13,10 +21,37 @@ import org.elasticsearch.rest.RestRequest;
  */
 public class SessionFilter extends RestFilter {
     private ESLogger log = Loggers.getLogger(this.getClass());
+    private LoginREST client;
+    static String base = "http://localhost:8080/skillcollider/rest";
+
+    public SessionFilter() {
+        ClientConfig cc = new ClientConfig();
+        cc.register(ClientFilter.class);
+        Client resource = ClientBuilder.newClient(cc);
+        client = WebResourceFactory.newResource(LoginREST.class, resource.target(base));
+    }
 
     @Override
     public void process(RestRequest request, RestChannel channel, RestFilterChain filterChain) throws Exception {
-        log.info("-- SessionFilter.process --");
+        String cookie = request.header("Kookie");
+        // TODO cash
+        log.info("-- SessionFilter.process -- " + request.header("Referer") + " ## " + request.header("Kookie"));
+        Method method = request.method();
+        if (method == Method.OPTIONS) {
+            log.info("++ options ++");
+            filterChain.continueProcessing(request, channel);
+            return;
+        }
+        try {
+            if (cookie != null && cookie.length() > 0) {
+                ClientFilter.setCookie(cookie);
+            }
+            client.checkSession();
+            log.info("+++process accepted");
+        } catch (Exception e) {
+            log.info("--- process rejected", e);
+            return;
+        }
         filterChain.continueProcessing(request, channel);
     }
 }
